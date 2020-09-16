e347487470f2dd7a54b13f12236809378136238a
Arif Balaev <balaev.arif@gmail.com>
2020-09-16 10:23:31 +0300

    Updates

---
+++ precache-manifest.09e980857d35326030d0eb38a7d8334c.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "28a6773b5c499aad6617384f4dc52bd2",
+    "url": "/travel/index.html"
+  },
+  {
+    "revision": "991551af07e2406c7888",
+    "url": "/travel/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "475856cbc2f57020a97a",
+    "url": "/travel/static/js/2.b26b7ce8.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/travel/static/js/2.b26b7ce8.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "991551af07e2406c7888",
+    "url": "/travel/static/js/main.535cdbba.chunk.js"
+  },
+  {
+    "revision": "75b646a37ef18011baae",
+    "url": "/travel/static/js/runtime-main.f5de2dd0.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/travel/static/media/logo.5d5d9eef.svg"
+  }
+]);
